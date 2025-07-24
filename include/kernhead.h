70b81c356fa750cf759d85ea8e23b0cde7dbc0f2
Matthatter693 <pra060903@gmail.com>
2025-07-24 17:33:11 +0530

    kernel headers

---
+++ include/kernhead.h
@@ -0,0 +1,12 @@
+#include<linux/init.h>
+#include<linux/module.h>
+#include<linux/kernel.h>
+#include<linux/cdev.h>
+#include<linux/device.h>
+#include<linux/fs.h>
+#include<linux/slab.h>
+#include<linux/uaccess.h>
+#include<linux/errno.h>
+#include<linux/mutex.h>
+
+extern void cbufr_init(void);
