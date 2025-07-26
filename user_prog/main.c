61f8bcbe1f5f08aff5f19fdd25837dce0df91ebf
Matthatter693 <pra060903@gmail.com>
2025-07-26 13:46:15 +0530

    added arg parsing

--- user_prog/main.c
+++ user_prog/main.c
@@ -3,58 +3,91 @@
 #include<fcntl.h>
 #include<unistd.h>
 #include<string.h>
+#include<getopt.h>
 
-int main()
+
+//Buffer sizes for Tx and Rx
+#define RBUF 100
+#define TBUF 100
+
+//flag for verbose args
+static int verbose_flag;
+
+int main(int argc,char *argv[])
 {
-int fd;
-char p[]="Hiii buddy!";
-char r[]="how are you?";
-char q[100];
+    int opt;
+    int fd;
+    char tx[TBUF];
+    char rx[RBUF];
 
-printf("str1:%s\n",p);
-printf("str2:%s\n",r);
+    //opening file descriptor
+    fd=open("/dev/cbuf-0",O_RDWR);
+    if(fd<0)
+    {
+        perror("failed to open the device");
+        return 0;
+    }
+    perror("open");
+    
 
-fd=open("/dev/cbuf-0",O_RDWR);
-if(fd<0)
-{
-    perror("failed to open the device");
-    return 0;
-}
+    while(1)
+    {
+        static struct option long_options[]=
+        {
+            {"verbose",no_argument,&verbose_flag,1},
+            {"brief",no_argument,&verbose_flag,0},
+            {"write",required_argument,0,'w'},
+            {"read",no_argument,0,'r'},
+            {"help",no_argument,0,'h'},
+            {0,0,0,0}
+        };
 
-if(write(fd,p,strlen(p)+1)<0)
-{
-    perror("failed to  write");
-    close(fd);
-    return 1;
-}
+        int option_index=0;
+        opt=getopt_long(argc,argv,"rhw:",long_options,&option_index);
+        if(opt==-1)
+            break;
 
-printf("message sent\n");
-
-if(write(fd,r,strlen(r)+1)<0)
-{
-    perror("failed to  write");
-    close(fd);
-    return 1;
-}
-
-printf("message sent\n");
-
-if(read(fd,q,sizeof(q))<0)
-{
-    perror("failed to  write");
-    close(fd);
-    return 1;
-}
-
-printf("Read from device:%s\n",q);
-
-if(read(fd,q,sizeof(q))<0)
-{
-    perror("failed to  write");
-    close(fd);
-    return 1;
-}
-printf("Read from device:%s\n",q);
-close(fd);
-
+        switch(opt)
+        {
+            case 0:
+                if(long_options[option_index].flag!=0)
+                    break;
+                break;
+            case 'r': 
+                if(read(fd,rx,sizeof(rx))<0)
+                {
+                    perror("failed to read");
+                    close(fd);
+                    return 1;
+                }
+                printf("Read from device:%s\n",rx);
+                close(fd);
+                break;
+            case 'w':
+                strcpy(tx,optarg);
+                if(write(fd,tx,strlen(tx)+1)<0)
+                {
+                    perror("failed to write");
+                    close(fd);
+                    return 1;
+                }
+                if(verbose_flag)
+                    printf("This works dawg\n");
+                printf("Message written successfully\n");
+                close(fd);
+                break;
+            case 'h':
+                printf("not planned yet\n");
+                close(fd);
+                break;
+            case '?':
+                printf("write correct arguments :(\n");
+                close(fd);
+                break;
+            default:
+                printf("Please provide some options\n");
+                close(fd);
+                exit(0);
+        }
+    }
 }
