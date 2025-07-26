46f1168331b0ccf1918a696adb2a6b710f8a3c40
Matthatter693 <pra060903@gmail.com>
2025-07-26 17:24:49 +0530

    added conditions and moved struct defs

--- include/kernhead.h
+++ include/kernhead.h
@@ -1,3 +1,6 @@
+#ifndef _KERN_HEAD
+#define _KERN_HEAD
+
 #include<linux/init.h>
 #include<linux/module.h>
 #include<linux/kernel.h>
@@ -9,4 +12,27 @@
 #include<linux/errno.h>
 #include<linux/mutex.h>
 
+#define CBUFMSG 50
+#define CBUFSZ 100
+
+//wrapping up the cdev struct
+
+struct cdev_data
+{
+    struct cdev cdev;
+    struct mutex lock;
+};
+
+//circ buffer struct
+
+typedef struct circ_buf
+{
+    char data[CBUFMSG][CBUFSZ];
+    int count;
+    int head;
+    int tail;
+}cbuf;
+
 extern void cbufr_init(void);
+
+#endif
